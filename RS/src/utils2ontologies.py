67d0ad09130e03c706d076e2693e0ebde8d409c5
Matilde Pós-de-Mina Pato <matpato@users.noreply.github.com>
2021-07-05 23:46:47 +0100

    add RS process

---
+++ RS/src/utils2ontologies.py
@@ -0,0 +1,146 @@
+
+###############################################################################
+#                                                                             #  
+# @author: Matilde Pato                                                       #  
+# @email: matilde.pato@gmail.com                                              #
+# @date: 31 Mar 2021                                                          #
+# @version: 1.0                                                               #  
+# Lasige - FCUL                                                               #
+#  (Adapted from Márcia Barros, Pedro Ruas, Diana Sousa)                      #  
+# @last update:                                                               #  
+#   version 1.1:                                                              #      
+#   (author:   )                                                              # 
+#                                                                             #   
+#                                                                             #  
+###############################################################################
+
+import os
+import urllib.request
+import requests
+import shutil
+from pathlib import Path
+
+import rdflib
+from rdflib import URIRef
+
+# --------------------------------------------------------------------------- #
+
+def get_owl(url, path):
+    '''
+    Download owl file and save it in a pre-defined folder. If <path>
+    does not exist script will create it in current working directory and save 
+    file in it
+    This is written specially to large file. The chunk size that we want to 
+    download at a time is specified
+    :param url: url of the ontology
+           path: folder where owl will be save 
+    '''
+    # create folder if it does not exist
+    if not os.path.exists(path):
+        os.makedirs(path)  
+
+    filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
+    file_path = os.path.join(path, filename)
+    
+    print(f'Downloading ... {filename}')
+
+    if url.startswith('ftp'):
+        with urllib.request.urlopen(url) as r:
+            with open(file_path, 'wb') as f:
+                shutil.copyfileobj(r, f)
+    else:
+        r = requests.get(url, stream = True)
+        if r.ok:
+            with open(file_path, "wb") as owl:
+                for chunk in r.iter_content(chunk_size = 1024 * 8):
+                    if chunk:
+                        owl.write(chunk)
+                        owl.flush()
+                        os.fsync(owl.fileno())
+        else:  # HTTP status code 4XX/5XX
+            print("Download failed: status code {}\n{}".format(r.status_code, r.text))
+
+# --------------------------------------------------------------------------- #
+
+def loading_items(is_chebi, is_do, is_go, is_hp):
+    '''
+    Loading ontologies to get the entities label
+    :param is_chebi, is_do, is_go, is_hp: boolean to represent which ontologies must be
+        loading  
+    :return chebi, do, go, hp: owl graph        
+    '''
+    path_chebi = '../data/ontologies/chebi.owl'
+    path_do = '../data/ontologies/doid.owl'
+    path_go = '../data/ontologies/go.owl'
+    path_hp = '../data/ontologies/hp.owl'
+    
+    # if chebi_lite.owl does not exists
+    if (not os.path.exists(path_chebi) and is_chebi):
+        url = "ftp://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.owl"
+        get_owl(url, path=os.path.dirname(path_chebi))
+
+    # if doid.owl does not exists
+    if (not os.path.exists(path_do) and is_do):
+        print('i am here')
+        url = " http://purl.obolibrary.org/obo/doid.owl"
+        get_owl(url, path=os.path.dirname(path_do))  
+    
+    # if go.owl does not exists
+    if (not os.path.exists(path_go) and is_go):
+        url = "http://purl.obolibrary.org/obo/go.owl"
+        get_owl(url, path=os.path.dirname(path_go))
+
+    # if hp.owl does not exists
+    if (not os.path.exists(path_hp) and is_hp):
+        url = "http://purl.obolibrary.org/obo/hp.owl"
+        get_owl(url, path=os.path.dirname(path_hp))
+
+    # create a Graph
+    chebi = do = go = hp = rdflib.Graph()
+    if is_chebi:
+        print('Loading ... chebi')    
+        chebi = load_ontology(path=path_chebi)
+    if is_do:
+        print('Loading ... doid')
+        do = load_ontology(path=path_do)
+    if is_go:
+        print('Loading ... go')
+        go = load_ontology(path=path_go)  
+    if is_hp:
+        print('Loading ... hp')
+        hp = load_ontology(path=path_hp)     
+    
+    return chebi, do, go, hp
+
+# --------------------------------------------------------------------------- #
+
+def load_ontology(path):
+
+    g = rdflib.Graph()
+    g.load(path)
+
+    return g
+
+# --------------------------------------------------------------------------- #
+
+def get_entities_labels(lst, chebi, do, go, hp):
+    '''
+    Get entities lables from http://purl.obolibrary.org/obo/ based on items prefix
+    :param  lst: list of entities
+            chebi, do, go, hp: items prefix of entities
+    :return label: entities label
+    '''
+    label = []
+    for id in lst: 
+
+        uri = URIRef('http://purl.obolibrary.org/obo/' + id)
+        if id.startswith('CHEBI'):
+            lab = chebi.label(uri)
+        elif id.startswith('GO'):
+            lab = go.label(uri)
+        elif id.startswith('HP'):
+            lab = hp.label(uri)
+        elif id.startswith('DO'):
+            lab = do.label(uri)
+        label.append(lab)
+    return label
---
+++ RS/src/utils2pubmed.py
@@ -0,0 +1,187 @@
+###############################################################################
+#                                                                             #  
+# @author: Matilde Pato (Adapted from André Lamurias)                         #  
+# @email: matilde.pato@gmail.com                                              #
+# @date: 31 Mar 2021                                                          #
+# @version: 1.0                                                               #  
+# Lasige - FCUL                                                               #
+#                                                                             #  
+# @last update:                                                               #  
+#   version 1.1:                                                              #      
+#   (author: )                                              #  
+###############################################################################
+#
+# This file get abstracts, authors, year based on PubMed
+#
+
+### -- PMID
+from metapub import PubMedFetcher
+from Bio import Entrez
+Entrez.email = 'matilde.pato@gmail.com'
+
+# --------------------------------------------------------------------------- #
+
+def get_pmid(pmcid):
+    '''
+    Return PubMed ID
+    
+    :param  pmcid:
+    :return pmid:         
+    '''
+    fetch = PubMedFetcher()
+    article = fetch.article_by_pmcid(pmcid)
+    return article.pmid
+
+# --------------------------------------------------------------------------- #
+
+def get_year_by_metapub(pmcid):
+    '''
+    Get PubMed year using metapub
+
+    :param  pmcid:
+    :return year         
+    '''
+    fetch = PubMedFetcher()
+    article = fetch.article_by_pmcid(pmcid)
+    return article.year
+
+# --------------------------------------------------------------------------- #
+
+def get_title_by_metapub(pmcid):
+    '''
+    Return PubMed ID
+    
+    :param  pmcid:
+    :return pmid:         
+    '''
+    fetch = PubMedFetcher()
+    #article = fetch.article_by_pmid(pmid)
+    article = fetch.article_by_pmcid(pmcid)
+    return article.title
+
+# --------------------------------------------------------------------------- #
+
+def get_title_by_bio(pmid):
+    ''' 
+    Get PubMed year using Bio
+
+    :param  pmid: PMID' article
+    :return year
+    '''
+    handle = Entrez.esummary(db="pubmed", id=pmid, retmode="xml")
+    record = Entrez.parse(handle)
+    return record['Title']
+
+# --------------------------------------------------------------------------- #
+
+def get_abstract_by_bio(pmid):
+    ''' 
+    Get PubMed year using Bio
+
+    :param  pmid: PMID' article
+    :return year
+    '''
+    handle = Entrez.efetch(db="pubmed", id=pmid, retmode="xml")
+    record = Entrez.read(handle)
+    handle.close()
+    article = record['PubmedArticle'][0]['MedlineCitation']
+    abstract = str()
+            
+    if 'Abstract' in article['Article'].keys(): # Some documents have no english abstract
+        eng_content = article['Article']['Abstract']
+    for element in eng_content['AbstractText']:
+        abstract += element
+    return abstract
+
+# --------------------------------------------------------------------------- #
+
+def get_year_by_metapub(pmcid):
+    '''
+    Get PubMed year using metapub
+
+    :param  pmcid:
+    :return year         
+    '''
+    fetch = PubMedFetcher()
+    article = fetch.article_by_pmcid(pmcid)
+    return article.year
+
+# --------------------------------------------------------------------------- #
+
+def get_year_by_bio(pmid):
+    ''' 
+    Get PubMed year using Bio
+
+    :param  pmid: PMID' article
+    :return year
+    '''
+    handle = Entrez.esummary(db="pubmed", id=pmid, retmode="xml")
+    record = Entrez.parse(handle)
+    return record['PubDate'].split()[0]
+
+# --------------------------------------------------------------------------- #
+
+def get_authors_by_bio(pmid):
+    ''' 
+    Get PubMed year using Bio
+
+    :param  pmid: PMID' article
+    :return authors dictionary
+    '''
+    handle = Entrez.efetch(db="pubmed", id=pmid, retmode="xml")
+    record = Entrez.read(handle)
+    handle.close()
+    article = record['PubmedArticle'][0]['MedlineCitation']
+    authors = list()
+    if 'AuthorList' in article['Article'].keys():# Get authors info: only consider articles with at least 1 author
+            
+        for author in article['Article']['AuthorList' ]:             
+            if 'ForeName' in author.keys() and 'LastName' in author.keys(): #Some authors are collective, e.g. 'ColectiveAuthor'
+                author_dict = {"first": author['ForeName'], "last": author['LastName']}            
+                authors.append(author_dict) 
+    return authors
+
+# --------------------------------------------------------------------------- #
+
+def get_authors_by_metapub(pmid):
+    '''
+    Get PubMed year using metapub
+
+    :param  pmcid:
+    :return year         
+    '''
+    fetch = PubMedFetcher()
+    article = fetch.article_by_pmid(pmid)
+    return article.authors    
+
+# --------------------------------------------------------------------------- #
+
+def check_authors_by_bio(pmid):
+    ''' 
+
+    :param  pmid: PMID' article
+    :return year
+    '''
+    handle = Entrez.efetch(db="pubmed", id=pmid, retmode="xml")
+    record = Entrez.read(handle)
+    handle.close()
+    article = record['PubmedArticle'][0]['MedlineCitation']
+    # Get authors info: only consider articles with at least 1 author
+    if 'AuthorList' in article['Article'].keys():
+        return True
+    return False
+
+
+# --------------------------------------------------------------------------- #
+
+def check_authors_by_metapub(pmcid):
+    ''' 
+
+    :param  pmid: PMID' article
+    :return year
+    '''
+    fetch = PubMedFetcher()
+    article = fetch.article_by_pmcid(pmcid)
+    if article.authors:
+        return True
+    return False
